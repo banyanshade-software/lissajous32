70acd4545d90844ccff7bec4c562f3638014f6b3
banyanshade-software <braun.daniel@free.fr>
2025-01-11 13:51:52 +0100

    gen partially ok

--- src/pwmdma.c
+++ src/pwmdma.c
@@ -13,18 +13,25 @@
 #include "itm_debug.h"
 
 
+
 #define USE_DMA 0
 #define MAXBUF 1024
 #define BASEFREQ 50000
 #define VALUEONE 631
 
-static uint16_t buf1[MAXBUF];
-static uint16_t buf2[MAXBUF];
-static uint16_t buflen1;
-static uint16_t buflen2;
+typedef struct sigbuf {
+	uint16_t buf[MAXBUF];
+	uint16_t buflen;
+	volatile uint16_t ridx;
+} sigbuf_t;
+
+#define NCHAN 2
+sigbuf_t gen[NCHAN];
 
 
-static uint16_t fillsine(uint16_t *buf, uint16_t freq)
+
+
+static void fillsine(sigbuf_t *buf, uint16_t freq)
 {
 	uint16_t buflen;
 
@@ -36,17 +43,18 @@ static uint16_t fillsine(uint16_t *buf, uint16_t freq)
 	for (int i=0; i<MAXBUF; i++) {
 		uint16_t k = i%(VALUEONE+1);
 		//...
-		buf1[i]=k;
+		buf->buf[i]=k;
 		if (k==VALUEONE) li = i;
 	}
-	if (!li) return buflen;
-	return li;
+	buf->buflen = li ? li : MAXBUF;
+	buf->ridx = 0;
+
 }
 
 static void fillbuf(void)
 {
-	buflen1 = fillsine(buf1, 5*100);
-	buflen2 = fillsine(buf2, 7*100);
+	fillsine(&gen[0], 3000);
+	fillsine(&gen[1], 4000);
 }
 
 static void pwmdma_start(void)
@@ -70,18 +78,29 @@ void pwm_tim_it(void)
 	if (0==(cnt%50000)) {
 		itm_debug1(DBG_TIM, "hop", cnt);
 	}
+	for (int i=0; i<NCHAN; i++) {
+		uint16_t v = gen[i].buf[gen[i].ridx];
+		gen[i].ridx++;
+		//pwm_timer->Instance->CCR4 = CNT_ON(t);
+		switch (i) {
+		case 0:
+			htim1.Instance->CCR1 = v;
+			break;
+		case 1:
+			htim1.Instance->CCR2 = v;
+			break;
+		case 2:
+			htim1.Instance->CCR3 = v;
+			break;
+		case 3:
+			htim1.Instance->CCR4 = v;
+			break;
+		}
+	}
 }
 
 void sigGenTask(void const * argument)
 {
-	itm_debug1(DBG_ERR, "gogogogogogogogogo", 0);
-	itm_debug1(DBG_ERR, "1234567890", 0);
-	itm_debug1(DBG_ERR, "go", 0);
-	itm_debug1(DBG_ERR, "go", 0);
-	itm_debug1(DBG_ERR, "go", 0);
-	itm_debug1(DBG_ERR, "go", 0);
-	itm_debug1(DBG_ERR, "go", 0);
-	itm_debug1(DBG_ERR, "go", 0);
 	itm_debug1(DBG_ERR, "go", 0);
 	fillbuf();
 	pwmdma_start();
